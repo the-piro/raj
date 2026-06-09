377abc591dfeae1aa9170c3a57c32600ca923e2f
the-piro <thezake0077@gmail.com>
2026-06-09 21:57:43 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,27 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+API_ID = 22724444 
+API_HASH = "d88e1dcdd8c5601832784adfc580442d"
+BOT_TOKEN = ""
+
+ADMINS = [7852142757 , 7838009892, 6573328336]
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            'log.txt',
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+
+logging.getLogger("aiohttp").setLevel(logging.WARNING)
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
