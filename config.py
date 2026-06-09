79f2fb15d30c75e55093ba46d6cf1c46346e14cc
the-piro <thezake0077@gmail.com>
2026-06-09 22:57:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ from logging.handlers import RotatingFileHandler
 API_ID = 22724444 
 API_HASH = "d88e1dcdd8c5601832784adfc580442d"
 BOT_TOKEN = "7813937360:AAFmy_0qxKw1F7RRDixPCEQoZjIyEgF09TI"
+DB_URL = "mongodb+srv://username:password@cluster.mongodb.net/?retryWrites=true&w=majority"
+DB_NAME = "DuplicateScanner"
 
 ADMINS = [7852142757 , 7838009892, 6573328336]
 
